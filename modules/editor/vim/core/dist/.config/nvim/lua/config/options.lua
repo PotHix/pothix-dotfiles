0375ca90348db6c79be7d0e1f779094ba866db47
PotHix <pothix@pothix.com>
2024-09-05 16:00:36 -0300

    Update lazyvim

--- modules/editor/vim/core/dist/.config/nvim/lua/config/options.lua
+++ modules/editor/vim/core/dist/.config/nvim/lua/config/options.lua
@@ -1,3 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- Set to "ruff_lsp" to use the old LSP implementation version.
+vim.g.lazyvim_python_ruff = "ruff"
